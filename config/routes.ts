2542a3ceccdc10964a9afd63c32191c7ab4b393f
NoraH1to <834053207@qq.com>
2021-05-04 15:06:40 +0800

    Initial commit

---
+++ config/routes.ts
@@ -0,0 +1,11 @@
+import { IRoute } from '@umijs/types';
+
+const routes: IRoute[] = [
+  {
+    path: '/',
+    component: '@/pages/index.tsx',
+    __isDynamic: true,
+  },
+];
+
+export default routes;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import styles from './index.less';
+const index: FC = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+};
+
+export default index;
